bff1ae3e86ab26bddb34d9c17543c6ce200b6fe8
baekinsoo <baikinsoo95@gmail.com>
2023-07-25 20:13:32 +0900

    ~ 스프링 데이터 섹션 5

--- src/main/groovy/study/datajpa/DataJpaApplication.groovy
+++ src/main/groovy/study/datajpa/DataJpaApplication.groovy
@@ -2,6 +2,8 @@ package study.datajpa
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableJpaAuditing
@@ -10,6 +12,11 @@ class DataJpaApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication, args)
+	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 
 }
